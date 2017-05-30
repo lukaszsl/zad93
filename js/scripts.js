949205c06341fb57822cc5015b8e3ebabc8444ee
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-30 22:37:27 +0000

    added .gitignore and scripts.js files

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.',
+	dinosaur = 'triceratops';
+
+var newText = text.replace('Velociraptor', dinosaur.toUpperCase()).substr(0, text.length/2);
